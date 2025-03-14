0c5a5c0f7ddd3532da8cff194a572e1141130c08
muwew <mungwew@gmail.com>
2025-03-14 18:16:43 +0800

    Update scoreboard upload

--- src/app/scoreboard/page.tsx
+++ src/app/scoreboard/page.tsx
@@ -28,7 +28,7 @@ function ScoreboardContents() {
             ];
 
             try {
-                const response = await fetch('/api/route', {
+                const response = await fetch('/api/upload', {
                     method: 'POST',
                     headers: { 'Content-Type': 'application/json' },
                     body: JSON.stringify({ participantId, stageScores })
