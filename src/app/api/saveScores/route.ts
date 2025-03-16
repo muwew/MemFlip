7342fa0101692ef8948455c35a8d0238bf297469
muwew <mungwew@gmail.com>
2025-03-17 02:08:18 +0800

    Revert to NEXT_PUBLIC

--- src/app/api/saveScores/route.ts
+++ src/app/api/saveScores/route.ts
@@ -5,10 +5,10 @@ export async function POST(req: Request) {
     try {
         console.log("Received request to /api/saveScores");
         console.log("🔍 Checking Supabase Env Variables...");
-        console.log("SUPABASE_URL:", process.env.SUPABASE_URL || "Not Found");
-        console.log("SUPABASE_ANON_KEY:", process.env.SUPABASE_ANON_KEY || "Not Found");
+        console.log("SUPABASE_URL:", process.env.NEXT_PUBLIC_SUPABASE_URL || "Not Found");
+        console.log("SUPABASE_ANON_KEY:", process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "Not Found");
         
-        if (!process.env.SUPABASE_URL || !process.env.SUPABASE_ANON_KEY) {
+        if (!process.env.NEXT_PUBLIC_SUPABASE_URL || !process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
             console.error("Supabase env variables are missing");
             return NextResponse.json({ error: "Supabase env variables are missing" }, { status: 500 });
         }
--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -1,12 +1,14 @@
 import { createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 
+
 export async function createClient() {
   const cookieStore = await cookies()
+  
 
   return createServerClient(
-    process.env.SUPABASE_URL!,
-    process.env.SUPABASE_ANON_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
         getAll() {
